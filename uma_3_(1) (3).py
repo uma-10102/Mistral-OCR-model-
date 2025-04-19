419c484959d304b4896ba82b892fea8feafe575d
uma-10102 <rm.uma@stat.christuniversity.in>
2025-04-19 11:04:06 +0530

    Update uma_3_(1) (3).py

--- uma_3_(1) (3).py
+++ uma_3_(1) (3).py
@@ -37,7 +37,7 @@ from dotenv import load_dotenv
 import os
 #load_dotenv()
 #PDF_PATH = r"C:\\Users\\myhom\\OneDrive\\Documents\\uma research main paper - Copy.pdf"#
-MISTRAL_API_KEY = "OHKrC2UQWOMRiXiyLNGW08HpzrJRbR9u"
+MISTRAL_API_KEY = "....."
 #api_key = userdata.get("MISTRAL_API_KEY")
 
 print(MISTRAL_API_KEY)
